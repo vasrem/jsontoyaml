7e30cccec43c9697c25391827815d9e92bd1f1ed
Vasilis Remmas <vasremm@gmail.com>
2019-08-08 10:57:02 +0200

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 use jsontoyaml::convert;
 
 extern crate clap;
-use clap::{Arg,App};
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("jsontoyaml")
         .version("0.1.0")
         .about("Converts JSON to YAML and vice versa using stdin")
-        .arg(Arg::with_name("reverse")
-             .short("r")
-             .long("reverse")
-             .help("YAML to JSON"))
+        .arg(
+            Arg::with_name("reverse")
+                .short("r")
+                .long("reverse")
+                .help("YAML to JSON"),
+        )
         .get_matches();
 
-	if matches.is_present("reverse"){
-		convert::yaml_to_json::run();
-	}else{
-		convert::json_to_yaml::run();
-	} 
-
+    if matches.is_present("reverse") {
+        convert::yaml_to_json::run();
+    } else {
+        convert::json_to_yaml::run();
+    }
 }
